2ac592c3d019f6479903e76b25c9d3f1e976b26d
avinash996 <akulaavinash996@gmail.com>
2025-01-03 10:51:06 +0530

    Welcome page

---
+++ Demo005.java
@@ -0,0 +1,11 @@
+package Core_java;
+public class Demo005 {
+    public static void main(String[] args){
+        String name = "avinash";
+        String pwd = "Avinash@123";
+        while((name == "avinash")&&(pwd == "Avinash@123")){
+        System.out.println("Hello avinash welcome to mruh");
+        break;
+        }
+    }
+}
