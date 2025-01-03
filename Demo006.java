bb82b5e0fa49ea7d214840c99d480fab536b33f1
avinash996 <akulaavinash996@gmail.com>
2025-01-03 10:59:18 +0530

    Login page

---
+++ Demo006.java
@@ -0,0 +1,12 @@
+public class Demo006 {
+    public static void main(String[] args){
+        String username = "user01";
+        String pwd = "user_01";
+        while((username == "user01")&&(pwd == "user_01")){
+            System.out.println("Succesfully login");
+            break;
+        }
+
+    }
+    
+}
