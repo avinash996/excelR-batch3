b45057ebda4d19a9670e4b71fd8299af4d4b9273
avinash996 <akulaavinash996@gmail.com>
2025-02-11 13:33:42 +0530

    username and password

---
+++ Demo006.java
@@ -0,0 +1,10 @@
+public class Demo006 {
+    public static void main(String[] args) {
+        String name = "akarsh123";
+        String pwd = "akarsh123";
+        while ((name == "akarsh123") && (pwd == "akarsh123")) {
+            System.out.println("Hello akarsh welcome to malla reddy university");
+            break;
+        }
+    }
+}
