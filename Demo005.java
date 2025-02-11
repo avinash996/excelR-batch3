0e6a00a0ddb9b1a3619255e794dd19062da38ded
avinash996 <akulaavinash996@gmail.com>
2025-02-11 13:30:25 +0530

    password

---
+++ Demo005.java
@@ -0,0 +1,10 @@
+public class Demo005 {
+    public static void main(String[] args) {
+        String username = "user01";
+        String pwd = "user_01";
+        while ((username == "user01") && (pwd == "user_01")) {
+            System.out.println("Hello user happy new year");
+            break;
+        }
+    }
+}
